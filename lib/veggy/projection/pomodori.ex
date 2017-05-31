9247e11342371c5d3884380c74deddc41fdbb233
gabriele.lana <gabriele.lana@cleancode.it>
2017-05-31 16:00:46 +0200

    pomodori-projection

--- lib/veggy/projection/pomodori.ex
+++ lib/veggy/projection/pomodori.ex
@@ -4,7 +4,44 @@ defmodule Veggy.Projection.Pomodori do
     events: ["PomodoroStarted", "PomodoroSquashed", "PomodoroCompleted"],
     identity: "pomodoro_id"
 
-  def process(_event, record) do
+  def process(%{"event" => "PomodoroStarted"} = event, record) do
     record
+    |> Map.put("pomodoro_id", event["pomodoro_id"])
+    |> Map.put("timer_id", event["aggregate_id"])
+    |> Map.put("started_at", event["_received_at"])
+    |> Map.put("status", "started")
+    |> Map.put("duration", event["duration"])
+  end
+  def process(%{"event" => "PomodoroCompleted"} = event, record) do
+    record
+    |> Map.put("completed_at", event["_received_at"])
+    |> Map.put("status", "completed")
+  end
+  def process(%{"event" => "PomodoroSquashed"} = event, record) do
+    record
+    |> Map.put("squashed_at", event["_received_at"])
+    |> Map.put("status", "squashed")
+  end
+
+  def query("pomodori-by-tag", %{"tag" => tag, "timer_id" => timer_id}) do
+    timer_id = Veggy.MongoDB.ObjectId.from_string(timer_id)
+    tags = String.split(tag, ",", trim: true) |> Enum.map(&String.trim/1) |> Enum.map(&String.downcase/1)
+    find(%{"timer_id" => timer_id, "tags" => %{"$in" => tags}})
+  end
+
+  def query("pomodori-of-the-day", %{"day" => day, "timer_id" => timer_id} = parameters) do
+    timer_id = Veggy.MongoDB.ObjectId.from_string(timer_id)
+    case Timex.parse(day, "{YYYY}-{0M}-{0D}") do
+      {:ok, day} ->
+        beginning_of_day =
+          day |> Timex.beginning_of_day |> Timex.to_datetime |> Veggy.MongoDB.DateTime.from_datetime
+        end_of_day =
+          day |> Timex.end_of_day |> Timex.to_datetime |> Veggy.MongoDB.DateTime.from_datetime
+        find(%{"started_at" => %{"$gte" => beginning_of_day, "$lte" => end_of_day},
+               "timer_id" => timer_id,
+              })
+      {:error, reason} ->
+        {:error, "day=#{parameters["day"]}: #{reason}"}
+    end
   end
 end
