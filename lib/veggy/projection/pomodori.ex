6aadd2e7971d602ae60870e499b745c1c7275c64
gabriele.lana <gabriele.lana@cleancode.it>
2017-12-03 15:24:28 +0100

    Show query pomodori-of-the-day

--- lib/veggy/projection/pomodori.ex
+++ lib/veggy/projection/pomodori.ex
@@ -7,4 +7,20 @@ defmodule Veggy.Projection.Pomodori do
   def process(_event, record) do
     record
   end
+
+  def query("pomodori-of-the-day", %{"day" => day, "timer_id" => timer_id} = parameters) do
+    timer_id = Veggy.MongoDB.ObjectId.from_string(timer_id)
+    case Timex.parse(day, "{YYYY}-{0M}-{0D}") do
+      {:ok, day} ->
+        beginning_of_day =
+          day |> Timex.beginning_of_day |> Timex.to_datetime |> Veggy.MongoDB.DateTime.from_datetime
+        end_of_day =
+          day |> Timex.end_of_day |> Timex.to_datetime |> Veggy.MongoDB.DateTime.from_datetime
+        find(%{"started_at" => %{"$gte" => beginning_of_day, "$lte" => end_of_day},
+               "timer_id" => timer_id,
+              })
+      {:error, reason} ->
+        {:error, "day=#{parameters["day"]}: #{reason}"}
+    end
+  end
 end
