4e7af7a0e44f672e8b35aca7e2b0cb2855ffc3ba
gabriele.lana <gabriele.lana@cleancode.it>
2018-09-29 16:55:06 +0200

    Make compiler happy with Veggy.Countdown.init

--- lib/veggy/countdown.ex
+++ lib/veggy/countdown.ex
@@ -9,17 +9,28 @@ defmodule Veggy.Countdown do
     GenServer.call(__MODULE__, {:start, duration, aggregate_id})
   end
 
+  def init(args) do
+    {:ok, args}
+  end
+
   def handle_call({:start, duration, aggregate_id}, _from, pomodori) do
-    pomodoro_id = Veggy.UUID.new
-    {:ok, reference} = :timer.send_after(duration, self(), {:completed, pomodoro_id, aggregate_id})
+    pomodoro_id = Veggy.UUID.new()
+
+    {:ok, reference} =
+      :timer.send_after(duration, self(), {:completed, pomodoro_id, aggregate_id})
+
     {:reply, {:ok, pomodoro_id}, Map.put(pomodori, pomodoro_id, reference)}
   end
 
   def handle_info({:completed, pomodoro_id, aggregate_id}, pomodori) do
     {_, pomodori} = Map.pop(pomodori, pomodoro_id)
-    Veggy.Aggregates.route(%{"command" => "CompletePomodoro",
-                             "pomodoro_id" => pomodoro_id,
-                             "timer_id" => aggregate_id})
+
+    Veggy.Aggregates.route(%{
+      "command" => "CompletePomodoro",
+      "pomodoro_id" => pomodoro_id,
+      "timer_id" => aggregate_id
+    })
+
     {:noreply, pomodori}
   end
 end
